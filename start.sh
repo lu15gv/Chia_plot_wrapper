ba0b96c6baf78f885f91d5050f547f13c017be11
Luis Gomez <>
2021-05-15 14:10:41 -0500

    Logs dir changed

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ set -e
 ###		MacOS
 ###		CHIA=/Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon/chia
 ### 1.- Set where you want to create the logs. This directory must exist!!
-		LOGS_DIR=C:/Users/luis1/Documents/chia/logs/plots.csv
+		LOGS_DIR=C:/Users/luis1/Documents/chia_logs
 ### 2.- This was set for 6 parallel queues.
 ### 3.- Set how many plots you want for each queue: 
 ### 	For example, queue_size=3 will produce 3 plots for each queue, that means
