eaf5343b67ad97440902fa78a8395676c3b854d1
Luis Gomez <>
2021-05-17 21:15:09 -0500

    Linux support added

--- start.sh
+++ start.sh
@@ -1,13 +1,34 @@
+BASEDIR=$(dirname "$0")
+
 set -e
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     		machine=Linux;;
+    Darwin*)    		machine=Mac;;
+    CYGWIN*)    		machine=Cygwin;;
+    MINGW64_NT-10.0*)	machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
 ###### Instructions ######
-### 2.- Set chia exectubale directory
+### 1.- Set chia exectubale directory
 ###		Windows
-		CHIA=C:/Users/luis1/AppData/Local/chia-blockchain/app-1.1.5/resources/app.asar.unpacked/daemon/chia.exe
+		if [ "$machine" = "Linux" ]; then
+			CHIA=C:/Users/luis1/AppData/Local/chia-blockchain/app-1.1.5/resources/app.asar.unpacked/daemon/chia.exe
+		fi
 ###		MacOS
-###		CHIA=/Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon/chia
-### 1.- Set where you want to create the logs. This directory must exist!!
-		LOGS_DIR=C:/Users/luis1/Documents/chia_logs
+		if [ "$machine" = "Mac" ]; then
+			CHIA=/Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon/chia
+		fi
+###		Linux
+		if [ "$machine" = "Linux" ]; then
+		    cd /usr/lib/chia-blockchain
+		    . ./activate
+		    chia init
+		fi
+### 2.- Only change it if you want to cahnge logs directory
+		LOGS_DIR=${BASEDIR}/logs/plots.csv
 ### 2.- This was set for 6 parallel queues.
 ### 3.- Set how many plots you want for each queue: 
 ### 	For example, queue_size=3 will produce 3 plots for each queue, that means
@@ -15,20 +36,22 @@ set -e
 		QUEUE_SIZE=3
 ### 4.- Other params, like temporal/final directory, threads and RAM, can be edited below.
 
-PLOTS_LOG=$LOGS_DIR/plots.csv
-if [ ! -f "$PLOTS_LOG" ]; then
-    touch $PLOTS_LOG
-	echo "ID,k,Temporal dir,Final dir,RAM,Threads,Start,End" >> $PLOTS_LOG
+if [ ! -f "$LOGS_DIR" ]; then
+    touch $LOGS_DIR
+	echo "ID,k,Temporal dir,Final dir,RAM,Threads,Start,End" >> $LOGS_DIR
 fi
 
-BASEDIR=$(dirname "$0")
-$BASEDIR/plot.sh id="A" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="B" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="C" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="D" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="F" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="G" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$PLOTS_LOG queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="A" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="B" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="C" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="D" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="E" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="F" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
 
 wait
 
 echo "All queues have finished"
+
+if [ "$machine" = "Linux" ]; then
+    deactivate
+fi
