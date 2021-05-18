d6104c334aeaa76d0574c78fb8373b8512451a68
Luis Gomez <>
2021-05-17 21:53:47 -0500

    Push notifications support added (with PushBack)

--- start.sh
+++ start.sh
@@ -26,6 +26,7 @@ esac
 		    cd /usr/lib/chia-blockchain
 		    . ./activate
 		    chia init
+		    CHIA=chia
 		fi
 ### 2.- Only change it if you want to cahnge logs directory
 		LOGS_DIR=${BASEDIR}/logs/plots.csv
@@ -34,19 +35,24 @@ esac
 ### 	For example, queue_size=3 will produce 3 plots for each queue, that means
 ###		6 parallel queues X 3 plots per queue = 18 plots in total.
 		QUEUE_SIZE=3
-### 4.- Other params, like temporal/final directory, threads and RAM, can be edited below.
+###	4.- Push notification
+		PUSH=false
+		if [ "$PUSH" = true ]; then
+			source push_keys.sh
+		fi
+### 5.- Other params, like temporal/final directory, threads and RAM, can be edited below.
 
 if [ ! -f "$LOGS_DIR" ]; then
     touch $LOGS_DIR
 	echo "ID,Queue,Description,k,Temporal dir,Final dir,RAM,Threads,Start,End" >> $LOGS_DIR
 fi
 
-$BASEDIR/plot.sh id="A" description="Running 6 in parallel" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="B" description="Running 6 in parallel" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="C" description="Running 6 in parallel" k=32 temp="D:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="D" description="Running 6 in parallel" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="E" description="Running 6 in parallel" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
-$BASEDIR/plot.sh id="F" description="Running 6 in parallel" k=32 temp="G:/" final="F:/" ram=4500 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="A" description="Running 11 in parallel" k=32 temp="D:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="B" description="Running 11 in parallel" k=32 temp="D:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="C" description="Running 11 in parallel" k=32 temp="D:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="D" description="Running 11 in parallel" k=32 temp="G:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="E" description="Running 11 in parallel" k=32 temp="G:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
+$BASEDIR/plot.sh id="F" description="Running 11 in parallel" k=32 temp="G:/" final="F:/" ram=5000 threads=2 log=$LOGS_DIR queue_size=$QUEUE_SIZE chia=$CHIA &
 
 wait
 
@@ -54,4 +60,12 @@ echo "All queues have finished"
 
 if [ "$machine" = "Linux" ]; then
     deactivate
+fi
+
+if [ "$PUSH" = true ]; then
+    curl https://api.pushback.io/v1/send \
+	-u "${ACCESS_TOKEN}:" \
+	-d "id=${USER_ID}" \
+	-d 'title=All queus have finished' \
+	-d 'body=:)'
 fi
