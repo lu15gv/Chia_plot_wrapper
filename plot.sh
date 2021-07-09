232a27b310c0e05f31df64a6f3276c2053327a4a
Luis Gomez <>
2021-07-08 21:57:49 -0500

    Missing farmer public key

--- plot.sh
+++ plot.sh
@@ -30,12 +30,11 @@ if [ -f "${BASEDIR}/keys.sh" ]; then
   source "${BASEDIR}/keys.sh"
 fi
 
+if [ -z "${FARMER_PUBLIC_KEY}" ]; then
+  FARMER_PUBLIC_KEY=aa711bae71d947a5c6806bc57ac5391722129e2ccf5cfdf068e324d65d723e0bef645d39ad8312785e36d95eb0127435
+fi
 
 if [ -z "${CONTRACT_ADDRESS}" ]; then
-  if [ -z "${FARMER_PUBLIC_KEY}" ]; then
-    FARMER_PUBLIC_KEY=aa711bae71d947a5c6806bc57ac5391722129e2ccf5cfdf068e324d65d723e0bef645d39ad8312785e36d95eb0127435
-  fi
-
   if [ -z "${POOL_PUBLIC_KEY}" ]; then
     POOL_PUBLIC_KEY=a72a54629d9090b7eca7d123ce62c3d46ce253cc2b4b5c86face704787a49b41dccb39f1b6f949e7f8467f5ef7f70c4c
   fi
@@ -67,11 +66,9 @@ else
   START_TIME=$(date '+%Y-%m-%d %H:%M:%S')
   echo "Mad Max"
   if [ -z "${CONTRACT_ADDRESS}" ]; then
-    echo "Solo"
     chia_plot -n $QUEUE -r $THREADS -t $TEMP -2 $TEMP_2 -d $FINAL -p $POOL_PUBLIC_KEY -f $FARMER_PUBLIC_KEY
   else
-    echo "Pool"
-    chia_plot -n $QUEUE -r $THREADS -t $TEMP -2 $TEMP_2 -d $FINAL -c $CONTRACT_ADDRESS
+    chia_plot -n $QUEUE -r $THREADS -t $TEMP -2 $TEMP_2 -d $FINAL -c $CONTRACT_ADDRESS -f $FARMER_PUBLIC_KEY
   fi
   END_TIME=$(date '+%Y-%m-%d %H:%M:%S')
   echo "$ID,"MadMax",$QUEUE,$DESCRIPTION,$K,$TEMP,$TEMP_2,$FINAL,"-",$THREADS,$START_TIME,$END_TIME" #>> "$LOG/plots.csv"
